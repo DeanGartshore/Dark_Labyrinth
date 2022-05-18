a3fbb752e402d028db1731476883661def2aaff9
DeanGartshore <0516507@fife.ac.uk>
2022-05-18 09:53:26 +0100

    Game Menus

---
+++ The Dark Labyrinth/Assets/Programs/Menu scripts/LoadMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+
+
+
+        SceneManager.LoadScene("Start Up Menu");
+
+
+    }
+
+}
